9edbb6ecea68d7961a20f407fda5fcd5f60559a1
MaryGothique <marypolly88@gmail.com>
2024-11-02 18:44:28 +0100

    roman_numeral

--- Roman_numeral_converter.js
+++ Roman_numeral_converter.js
@@ -9,9 +9,9 @@ convertButton.addEventListener("click", function() {
   if (number.value.trim() === "") {
     // Se è vuoto, mostra il messaggio di errore nell'output
     output.textContent = "Please enter a valid number";
-  } else if (number.value.trim() < 1) { // Cambiato "numberInput" con "number"
+  } else if (number.value.trim() < 1) { 
     output.textContent = "Please enter a number greater than or equal to 1"; 
-  } else if (number.value.trim() > 3999) { // Cambiato "<= 4000" con "> 3999"
+  } else if (number.value.trim() > 3999) { 
     output.textContent = "Please enter a number less than or equal to 3999";
   } else if (number.value.trim() ==9) { 
     output.textContent = "IX";
@@ -25,7 +25,7 @@ convertButton.addEventListener("click", function() {
     output.textContent = "MMMCMXCIX";
   }
   else {
-    // Qui puoi continuare con la logica di conversione dei numeri romani
+    
     output.textContent = "Conversion logic goes here"; // Placeholder per la logica di conversione
   }
 });
